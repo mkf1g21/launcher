fd5194e5c1d0f571fb14de9a9e06c3a31e2d4a92
mkf1g21 <97884860+mkf1g21@users.noreply.github.com>
2022-01-17 10:56:56 +0000

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 10:37:58 2022
+
+@author: michael
+"""
+
+import launcher
+l=launcher.Launcher()
+l.plot_angle_v_range(10)
